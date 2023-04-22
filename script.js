142c11123ec8858f180b226768d680ee1475ff75
skfarukhossain1998 <127541802+skfarukhossain1998@users.noreply.github.com>
2023-04-22 23:53:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-
+let yourArray = [
+  [
+    {
+      name: "Faruk",
+      age: 22,
+	status:true
+    },
+    {
+      name: "Faruk",
+      age: 22,
+	status:true
+    },
+	  {
+      name: "Faruk",
+      age: 22,
+	status:true
+    },
+	  {
+      name: "Saruk",
+      age: 42,
+	status:false
+    },
+	  {
+      name: "Faruk",
+      age: 23,
+	status:false
+    },
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    }
+  ]
+];
